50b8280f8cc9c2ca2e8953506b2fd423e2f5aa56
codergreg <greg.lin@gmail.com>
2013-11-18 09:46:15 -0800

    rtb/cookbooks: adds/removes packages

--- rtb/recipes/default.rb
+++ rtb/recipes/default.rb
@@ -9,10 +9,22 @@
 #
 pkglist = [
   'mytop', 'fabric', 'sysstat',  'emacs23-nox', 'nginx', 'mysql-client', 'git',
-  'php-pear', 'php-apc', 'php5-fpm', 'php5-curl', 'php5-cli', 'php-elisp', 'php5-mysql']
+  'php-pear', 'php-apc', 'php5-fpm', 'php5-curl', 'php5-cli', 'php-elisp', 'php5-mysql',
+  'memcached', 'php5-memcached', 'php5-memcache',
+  'redis-server', 'make', 'openjdk-7-jre-headless']
+
+remlist = [
+  'apparmor'
+]
 
 pkglist.each do |pkg|
   package pkg do
     action :install
+  end
+end
+
+remlist.each do |pkg|
+  package pkg do
+    action :purge
   end
 end
